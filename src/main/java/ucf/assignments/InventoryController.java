633e6622b5258947cb6820a457dda690be9cf0e8
Akwill47 <wil.weat@hotmail.com>
2021-07-22 10:10:25 -0400

    added functions/controller stuff

--- src/main/java/ucf/assignments/InventoryController.java
+++ src/main/java/ucf/assignments/InventoryController.java
@@ -1,5 +1,150 @@
 package ucf.assignments;
 
-public class InventoryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class InventoryController {
+    InventoryFunctions func = new InventoryFunctions();
+    @FXML
+    TextField addName;
+    @FXML
+    TextField addSerial;
+    @FXML
+    TextField addValue;
+    @FXML
+    ListView<String> inventoryList;
+
+    ArrayList<HashMap<String,String>>workList = new ArrayList<>();//main working list
+    ObservableList<String> observList = FXCollections.observableArrayList();
+
+    private void refresh(){
+        //resets the textfields
+        addValue.setText(null);
+        addName.setText(null);
+        addSerial.setText(null);
+    }
+
+    public void AddListItem(ActionEvent actionEvent) {
+        func.addItem(workList, addName.getText(),addSerial.getText(),addValue.getText());
+        observList.add(func.prettyString(workList.get(workList.size()-1)));
+        inventoryList.setItems(observList);
+
+        refresh();
+    }
+
+    public void removeItem(ActionEvent actionEvent) {
+        int index = inventoryList.getSelectionModel().getSelectedIndex();
+        if(index>=0){
+            func.removeItem(workList,index);
+            observList.remove(index);
+        }
+    }
+
+    public void editName(ActionEvent actionEvent) {
+        int index = inventoryList.getSelectionModel().getSelectedIndex();
+        if(index>=0){
+            func.editItemName(workList,index,addName.getText());
+        }
+        observList.set(index,func.prettyString(workList.get(index)));
+        refresh();
+    }
+
+    public void editSerial(ActionEvent actionEvent) {
+        int index = inventoryList.getSelectionModel().getSelectedIndex();
+        if(index>=0){
+            func.editSerialNumber(workList,index,addSerial.getText());
+        }
+        observList.set(index,func.prettyString(workList.get(index)));
+        refresh();
+    }
+
+    public void editValue(ActionEvent actionEvent) {
+        int index = inventoryList.getSelectionModel().getSelectedIndex();
+        if(index>=0){
+            func.editValue(workList,index,addValue.getText());
+        }
+        observList.set(index,func.prettyString(workList.get(index)));
+        refresh();
+    }
+
+    public void clearList(ActionEvent actionEvent) {
+        func.clearList(workList);
+        observList.clear();
+        inventoryList.setItems(observList);
+    }
+
+    public void sortValue(ActionEvent actionEvent) {
+        ArrayList<HashMap<String,String>> tempValue = new ArrayList<>();
+        func.sortByValue(tempValue,workList);
+
+        ObservableList<String> valueList = FXCollections.observableArrayList();
+        for(int i=0;i<tempValue.size();i++){
+            valueList.add(func.prettyString(tempValue.get(i)));
+        }
+        inventoryList.setItems(valueList);
+    }
+
+    public void sortSerial(ActionEvent actionEvent) {
+        ArrayList<HashMap<String,String>> tempSerial = new ArrayList<>();
+        func.sortBySerialNumber(tempSerial,workList);
+
+        ObservableList<String> serialList = FXCollections.observableArrayList();
+        for(int i=0;i<tempSerial.size();i++){
+            serialList.add(func.prettyString(tempSerial.get(i)));
+        }
+        inventoryList.setItems(serialList);
+    }
+
+    public void sortName(ActionEvent actionEvent) {
+        ArrayList<HashMap<String,String>> tempName = new ArrayList<>();
+        func.sortByName(tempName,workList);
+
+        ObservableList<String> nameList = FXCollections.observableArrayList();
+        for(int i=0;i<tempName.size();i++){
+            nameList.add(func.prettyString(tempName.get(i)));
+        }
+        inventoryList.setItems(nameList);
+    }
+
+    public void saveCurrent(ActionEvent actionEvent) {
+    }
+
+    public void loadOne(ActionEvent actionEvent) {
+    }
+
+
+    public void searchSerial(ActionEvent actionEvent) {
+        ArrayList<HashMap<String,String>> searchList = new ArrayList<>();
+        String searchKey = null;
+        func.searchBySerialNumber(searchList,workList,searchKey);
+        ObservableList<String> searchObservableList = FXCollections.observableArrayList();
+        for(int i=0;i<searchList.size();i++){
+           searchObservableList.add(func.prettyString(searchList.get(i)));
+        }
+        inventoryList.setItems(searchObservableList);
+    }
+
+    public void searchName(ActionEvent actionEvent) {
+        ArrayList<HashMap<String,String>> searchList = new ArrayList<>();
+        String searchKey = null;
+        func.searchByName(searchList,workList,searchKey);
+        ObservableList<String> searchObservableList = FXCollections.observableArrayList();
+        for(int i=0;i<searchList.size();i++){
+            searchObservableList.add(func.prettyString(searchList.get(i)));
+        }
+        inventoryList.setItems(searchObservableList);
+    }
+
+    public void sortAll(ActionEvent actionEvent) {
+        inventoryList.setItems(observList);
+    }
 }
--- src/main/java/ucf/assignments/InventoryFunctions.java
+++ src/main/java/ucf/assignments/InventoryFunctions.java
@@ -4,36 +4,61 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class InventoryFunctions {
-    public String addItem(ArrayList<HashMap<String,String>> list,String info){
+    public String prettyString(HashMap<String,String> item){
+        return "Serial Number: " + item.get("serial") + "\tName: " + item.get("name") + "\t\t\t\t\tValue: " + item.get("value");
+    }
+    public String addItem(ArrayList<HashMap<String,String>> list,String name,String serial,String value){
         //cannot duplicate serial number
+        HashMap<String,String> newItem = new HashMap<>();
+        newItem.put("serial",serial);
+        newItem.put("name",name);
+        newItem.put("value",value);
+        list.add(newItem);
         return "Item successfully added";
     }
     public String removeItem(ArrayList<HashMap<String,String>> list,int index){
+        list.remove(index);
         return "Item successfully removed";
     }
-    public String editValue(ArrayList<HashMap<String,String>> list,int index, double value){
+    public String editValue(ArrayList<HashMap<String,String>> list,int index, String value){
+        list.get(index).put("value",value);
         return "Item value changed";
     }
-    public String editSerialNumber(ArrayList<HashMap<String,String>> list,int index,double serialNumber){
+    public String editSerialNumber(ArrayList<HashMap<String,String>> list,int index,String serialNumber){
         //cannot duplicate serial number
+        list.get(index).put("serial",serialNumber);
         return "Item serial number changed";
     }
     public String editItemName(ArrayList<HashMap<String,String>> list,int index,String newName){
+        list.get(index).put("name",newName);
         return "Item name changed";
     }
-    public String sortByValue(ArrayList<HashMap<String,String>> list){
+    public String sortByValue(ArrayList<HashMap<String,String>>valueSort, ArrayList<HashMap<String,String>> list){
+
         return "Items sorted by value";
     }
-    public String sortBySerialNumber(ArrayList<HashMap<String,String>> list){
+    public String sortBySerialNumber(ArrayList<HashMap<String,String>>serialSort, ArrayList<HashMap<String,String>> list){
+
         return "Items sorted by serial number";
     }
-    public String sortByName(ArrayList<HashMap<String,String>> list){
+    public String sortByName(ArrayList<HashMap<String,String>>nameSort, ArrayList<HashMap<String,String>> list){
+
         return "Items sorted by name";
     }
-    public String searchByName(ArrayList<HashMap<String,String>> list,String searchKey){
+    public String searchByName(ArrayList<HashMap<String,String>>nameSearch, ArrayList<HashMap<String,String>> list,String searchKey){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).get("name").equals(searchKey)) {
+                nameSearch.add(list.get(i));
+            }
+        }
         return "Item searched by name";
     }
-    public String searchBySerialNumber(ArrayList<HashMap<String,String>> list,String searchKey){
+    public String searchBySerialNumber(ArrayList<HashMap<String,String>>serialSearch, ArrayList<HashMap<String,String>> list,String searchKey){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).get("serial").equals(searchKey)) {
+                serialSearch.add(list.get(i));
+            }
+        }
         return "Item searched by serial number";
     }
     public String saveInventory(ArrayList<HashMap<String,String>> list,String fileName,String fileLocation){
@@ -44,5 +69,14 @@ public class InventoryFunctions {
     public String loadInventory(ArrayList<HashMap<String,String>> list,String fileName,String fileLocation){
         //user provides filename and file location
         return "Inventory Items loaded from file";
+    }
+    public String clearList(ArrayList<HashMap<String,String>> list){
+        //reference overall list
+
+        //remove each index one by one
+        for(int i=list.size();i>0;i--){
+            list.remove(i-1);
+        }
+        return "List successfully cleared";
     }
 }
